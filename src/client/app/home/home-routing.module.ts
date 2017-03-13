d573063f6c95d3db690aa6a3cf57269953c4320d
Santiago J. Said <said.santiago@gmail.com>
2017-03-13 09:30:33 -0300

    [MOD] In root url fire login component

--- src/client/app/home/home-routing.module.ts
+++ src/client/app/home/home-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from '../login/login.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: '', component: HomeComponent }
+      { path: '', component: LoginComponent }
     ])
   ],
   exports: [RouterModule]
