0bbc8f8f5ac872663a05dd04164515a5ebf52678
Santiago J. Said <said.santiago@gmail.com>
2017-03-13 07:50:08 -0300

    [MOD] Domain of incoming pickings

--- src/client/app/incomings/open.service.ts
+++ src/client/app/incomings/open.service.ts
@@ -10,7 +10,7 @@ export class OpenService {
   
   }
   get_pickings_in(): Promise<Picking[]> {
-    return this.odoo.searchRead('stock.picking.in', [["type", "=", "in"]], ["name", "partner_id"])
+    return this.odoo.searchRead('stock.picking.in', [["type", "=", "in"], ["state", "in", ["assigned",]]], ["name", "partner_id"])
       .then( (pickings: any) => {
         console.log("[OpenService]: ", pickings);
         return pickings.records;
